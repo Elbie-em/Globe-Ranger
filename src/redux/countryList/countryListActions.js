ed92064884dc75e1d23c4100cb6bc6bcdf90a9cc
Elbie-em <emoonga1993@gmail.com>
2020-12-18 01:09:45 +0200

    Create country list action creators

--- src/redux/countryList/countryListActions.js
+++ src/redux/countryList/countryListActions.js
@@ -1,2 +1,21 @@
 import {FETCH_COUNTRIES_REQUEST, FETCH_COUNTRIES_SUCCESS, FETCH_COUNTRIES_FAILURE} from './countryListTypes'
 
+export const fetchCountriesRequest = () => {
+  return {
+    type: FETCH_COUNTRIES_REQUEST
+  }
+}
+
+export const fetchCountriesSuccess = (countries) => {
+  return {
+    type: FETCH_COUNTRIES_SUCCESS,
+    payload: countries
+  }
+}
+
+export const fetchCountriesFailure = (error) => {
+  return {
+    type: FETCH_COUNTRIES_FAILURE,
+    payload: error
+  }
+}
