a5bce784afc86c1023599a2928660d82a14f302e
Elbie-em <emoonga1993@gmail.com>
2020-12-18 01:15:15 +0200

    Add initial state constant

--- src/redux/countryList/countryListReducer.js
+++ src/redux/countryList/countryListReducer.js
@@ -1,2 +1,8 @@
 import {FETCH_COUNTRIES_REQUEST, FETCH_COUNTRIES_SUCCESS, FETCH_COUNTRIES_FAILURE} from './countryListTypes'
 
+const initialState = {
+  loading: true,
+  data: [],
+  errorMsg: ''
+}
+
