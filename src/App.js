35f629d55303e6f376a40eef820d26c6905e96c7
Elbie-em <emoonga1993@gmail.com>
2020-12-18 00:43:04 +0200

    Set up routes for app navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import {BrowserRouter,Switch, Route, NavLink, Redirect} from 'react-router-dom'
 import './App.css';
+import CountryContainer from './containers/CountryContainer';
+import CountryListContainer from './containers/CountryListContainer';
 
 function App() {
   return (
-    
+    <BrowserRouter>
       <div className="App">
-        
+      <Switch>
+        <Route path={"/"} exact component={CountryListContainer}/>
+        <Route path={"/country/:country"} exact component={CountryContainer}/>
+        <Redirect to={"/"} />
+      </Switch>
       </div>
-    
+    </BrowserRouter>
   )
 }
 
