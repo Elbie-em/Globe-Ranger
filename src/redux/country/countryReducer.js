70efeef1827c3c029714954d6a4ebbcf5157920a
Elbie-em <emoonga1993@gmail.com>
2020-12-22 15:39:24 +0200

    Create country reducer

---
+++ src/redux/country/countryReducer.js
@@ -0,0 +1,34 @@
+import {FETCH_COUNTRY_REQUEST, FETCH_COUNTRY_SUCCESS, FETCH_COUNTRY_FAILURE} from './countryTypes'
+
+const initialState = {
+  loading: true,
+  data: [],
+  errorMsg: ''
+}
+
+const countryReducer = (state = initialState, action) => {
+  switch(action.type){
+    case FETCH_COUNTRY_REQUEST:
+      return {
+        ...state,
+        loading: true
+      }
+    case FETCH_COUNTRY_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+        errorMsg: '',
+      }
+    case FETCH_COUNTRY_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        data: [],
+        errorMsg: action.payload,
+      }
+    default: return state
+  }
+}
+
+export default countryReducer
