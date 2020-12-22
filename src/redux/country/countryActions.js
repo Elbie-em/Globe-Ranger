d86f4ea706c680842fee5c1ba44a081defa9ae36
Elbie-em <emoonga1993@gmail.com>
2020-12-22 15:39:06 +0200

    Import axios, create unique action creator

--- src/redux/country/countryActions.js
+++ src/redux/country/countryActions.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import {FETCH_COUNTRY_REQUEST, FETCH_COUNTRY_SUCCESS, FETCH_COUNTRY_FAILURE} from './countryTypes'
 
 export const fetchCountryRequest = () => {
@@ -17,6 +18,20 @@ export const fetchCountryFailure = (error) => {
   return {
     type: FETCH_COUNTRY_FAILURE,
     payload: error
+  }
+}
+
+export const fetchCountry = (name) => {
+  return (dispatch) => {
+    dispatch(fetchCountryRequest())
+    axios.get(`https://restcountries.eu/rest/v2/name/${name}`)
+    .then(response => {
+      const countryData = response.data
+      dispatch(fetchCountrySuccess(countryData))
+    }).catch(error => {
+      const errorMsg = error.message
+      dispatch(fetchCountryFailure(errorMsg))
+    })
   }
 }
 
