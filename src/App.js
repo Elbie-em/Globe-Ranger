8b7ad998525045e22bb30f6b5c745df62465163f
Elbie-em <emoonga1993@gmail.com>
2020-12-22 11:08:47 +0200

    Import Provider and store, pass store to Provider store prop

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import {Provider} from 'react-redux'
+import store from './redux/store'
 import {BrowserRouter,Switch, Route, NavLink, Redirect} from 'react-router-dom'
 import './App.css';
 import CountryContainer from './containers/CountryContainer';
@@ -6,16 +8,18 @@ import CountryListContainer from './containers/CountryListContainer';
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-      <nav className="navbar navbar-dark bg-dark p-3">
-        <NavLink to={"/"} className="navbar-brand">Globe Ranger</NavLink>
-      </nav>
-      <Switch>
-        <Route path={"/"} exact component={CountryListContainer}/>
-        <Route path={"/country/:country"} exact component={CountryContainer}/>
-        <Redirect to={"/"} />
-      </Switch>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <nav className="navbar navbar-dark bg-dark p-3">
+            <NavLink to={"/"} className="navbar-brand">Globe Ranger</NavLink>
+          </nav>
+          <Switch>
+            <Route path={"/"} exact component={CountryListContainer}/>
+            <Route path={"/country/:country"} exact component={CountryContainer}/>
+            <Redirect to={"/"} />
+          </Switch>
+        </div>
+      </Provider>
     </BrowserRouter>
   )
 }
