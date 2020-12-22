bf33985c6e3c7a92b7b2b7a24d7e1f26fbe40372
Elbie-em <emoonga1993@gmail.com>
2020-12-22 14:24:17 +0200

    Add country action creators

--- src/redux/country/countryActions.js
+++ src/redux/country/countryActions.js
@@ -1,2 +1,22 @@
 import {FETCH_COUNTRY_REQUEST, FETCH_COUNTRY_SUCCESS, FETCH_COUNTRY_FAILURE} from './countryTypes'
 
+export const fetchCountryRequest = () => {
+  return {
+    type: FETCH_COUNTRY_REQUEST
+  }
+}
+
+export const fetchCountrySuccess = (country) => {
+  return {
+    type: FETCH_COUNTRY_SUCCESS,
+    payload: country
+  }
+}
+
+export const fetchCountryFailure = (error) => {
+  return {
+    type: FETCH_COUNTRY_FAILURE,
+    payload: error
+  }
+}
+
