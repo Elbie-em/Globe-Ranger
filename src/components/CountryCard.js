a0dbc9c4c6fe9c442cabda51cfe577a0be44e459
Elbie-em <emoonga1993@gmail.com>
2020-12-22 16:26:55 +0200

    Apply numberWithCommas function

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { numberWithCommas } from '../dataOperations/conversions'
 
 const CountryCard = ({country}) => {
   return (
     <div className="card mt-5" style={{width: 350}}>
-      <img src={country.flag} className="flag" alt="..."/>
+      <img src={country.flag} className="flag-sm" alt="..."/>
         <div className="card-body">
           <h3 className="card-title">{country.name}</h3>
-          <p>Population: {country.population}</p>
+          <p>Population: {numberWithCommas(country.population)}</p>
           <p>Region: {country.region}</p>
           <p>Capital: {country.capital}</p>
           <Link className="btn btn-info" to={`/country/${country.name}`}>View Details</Link>
