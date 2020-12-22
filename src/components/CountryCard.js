63e15d57e8ce3004823823eed6bf655c82d9a853
Elbie-em <emoonga1993@gmail.com>
2020-12-22 13:21:19 +0200

    Import link component from react-router-dom and pass it to country card component

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const CountryCard = ({country}) => {
   return (
     <div className="card mt-5" style={{width: 350}}>
-      <img src={country.flag} className="card-img-top" alt="..."/>
+      <img src={country.flag} className="flag" alt="..."/>
         <div className="card-body">
           <h3 className="card-title">{country.name}</h3>
           <p>Population: {country.population}</p>
           <p>Region: {country.region}</p>
           <p>Capital: {country.capital}</p>
+          <Link className="btn btn-info" to={`/country/${country.name}`}>View Details</Link>
         </div>
     </div>
   )
