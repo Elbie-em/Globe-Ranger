0c717b61fda308945e0291253c16262b8c1ee30e
Elbie-em <emoonga1993@gmail.com>
2020-12-22 11:11:18 +0200

    Import axios

--- src/redux/countryList/countryListActions.js
+++ src/redux/countryList/countryListActions.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import {FETCH_COUNTRIES_REQUEST, FETCH_COUNTRIES_SUCCESS, FETCH_COUNTRIES_FAILURE} from './countryListTypes'
 
 export const fetchCountriesRequest = () => {
@@ -19,3 +20,4 @@ export const fetchCountriesFailure = (error) => {
     payload: error
   }
 }
+
