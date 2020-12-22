072a99827672e054c68c333f1babb70e8d56c5a7
Elbie-em <emoonga1993@gmail.com>
2020-12-22 15:41:12 +0200

    Start build on country container

--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {connect} from 'react-redux'
+import {fetchCountry} from '../redux'
+import _ from "lodash"
 
- const CountryContainer  = () => {
+ const CountryContainer  = ({match,country,fetchCountry,}) => {
+   const countryName = match.params.country
+   useEffect(() => {
+    fetchCountry(countryName)
+  },[])
+
+  const showData = () => {
+    if(!_.isEmpty(country.data[0])){
+      return <h1 className="text-white">Data found</h1>
+    }
+
+    if(country.loading){
+      return <p className="text-white">Loading....</p>
+    }
+    if(country.errorMsg !== "") {
+    return <p>{country.errorMsg}</p>
+    }
+
+    return <p className="text-white">Error Loading Country Details</p>
+
+  }
+
   return (
     <div>
-      <h1>Country</h1>
+      {showData()}
     </div>
   )
 }
 
-export default CountryContainer
+const mapStateToProps = state => {
+  return {
+    country: state.country
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCountry : (name) => dispatch(fetchCountry(name))
+  }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(CountryContainer)
