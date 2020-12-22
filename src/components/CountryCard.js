8f43ba800b101620aee9c14c5cb24628cd0ac602
Elbie-em <emoonga1993@gmail.com>
2020-12-22 16:38:04 +0200

    Post styling in CountryCard component

--- src/components/CountryCard.js
+++ src/components/CountryCard.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { numberWithCommas } from '../dataOperations/conversions'
 
-const CountryCard = ({country}) => {
+const CountryCard = ({ country }) => {
   return (
-    <div className="card mt-5" style={{width: 350}}>
-      <img src={country.flag} className="flag-sm" alt="..."/>
+    <div className="mx-auto">
+      <div className="card m-3 text-white bg-dark" style={{ width: 320 }}>
+        <img src={country.flag} className="flag-sm rounded" alt="..." />
         <div className="card-body">
           <h3 className="card-title">{country.name}</h3>
           <p>Population: {numberWithCommas(country.population)}</p>
@@ -13,6 +14,7 @@ const CountryCard = ({country}) => {
           <p>Capital: {country.capital}</p>
           <Link className="btn btn-info" to={`/country/${country.name}`}>View Details</Link>
         </div>
+      </div>
     </div>
   )
 }
--- src/components/CountryDetail.js
+++ src/components/CountryDetail.js
@@ -6,21 +6,21 @@ const CountryDetail = ({countryDetails}) => {
     <div className="text-center">
        <h1 className="text-white brand-font">{countryDetails.name}</h1>
        <br />
-      <img src={countryDetails.flag} className="flag-lg mx-auto" alt="..."/>
-      <div className="country-info text-white">
-        <div className="align-right p-5">
-          <h4>Native Name: {countryDetails.nativeName}</h4>
-          <h4>Capital: {countryDetails.capital}</h4>
-          <h4>Population: {numberWithCommas(countryDetails.population)}</h4>
-          <h4>Region: {countryDetails.region}</h4>
+      <img src={countryDetails.flag} className="flag-lg mx-auto" alt="..."/>$ 
+        <div className="row text-white w-75 mx-auto">
+          <div className="align-left col-sm-12 col-md-6 p-5">
+            <h4>Native Name: {countryDetails.nativeName}</h4>
+            <h4> Capital: {countryDetails.capital}</h4>
+            <h4>Population: {numberWithCommas(countryDetails.population)}</h4>
+            <h4>Region: {countryDetails.region}</h4>
+          </div>
+          <div className="align-left col-sm-12 col-md-6 p-5">
+            <h4>Sub Region: {countryDetails.subRegion ?countryDetails.subRegion: "N/A" }</h4>
+            <h4>Top Level Domain: {countryDetails.topLevelDomain}</h4>
+            <h4>Currency: {countryDetails.currencies[0].name} - {countryDetails.currencies[0].symbol}</h4>
+            <h4>Language: {countryDetails.languages[0].name}</h4>
+          </div>
         </div>
-        <div className="align-left p-5">
-          <h4>Sub Region: {countryDetails.subRegion ?countryDetails.subRegion: "N/A" }</h4>
-          <h4>Top Level Domain: {countryDetails.topLevelDomain}</h4>
-          <h4>Currency: {countryDetails.currencies[0].name} - {countryDetails.currencies[0].symbol}</h4>
-          <h4>Language: {countryDetails.languages[0].name}</h4>
-        </div>
-      </div>
     </div>
     
   )
