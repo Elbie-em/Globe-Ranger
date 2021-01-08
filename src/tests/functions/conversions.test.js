6ba5daab52f974071580e99f4dafecac101b6643
Elbie-em <emoonga1993@gmail.com>
2021-01-08 20:28:10 +0200

    Fix lint errors

--- src/tests/functions/conversions.test.js
+++ src/tests/functions/conversions.test.js
@@ -1,8 +1,8 @@
-import numberWithCommas from '../../dataOperations/conversions'
+import numberWithCommas from '../../dataOperations/conversions';
 
 describe('Testing the conversions module', () => {
   test('it splits an integer to have a string with commas', () => {
-      const population = 22387000
-      expect(numberWithCommas(population)).toBe("22,387,000")
-  })
-})
+    const population = 22387000;
+    expect(numberWithCommas(population)).toBe('22,387,000');
+  });
+});
